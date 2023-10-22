8797fffaf8e58d8ad86d152159d09808a910faea
MrJanHorak <janny_b_goode@hotmail.com>
2023-10-22 16:03:20 -0400

    create Subscribe component

--- app/page.tsx
+++ app/page.tsx
@@ -1,48 +1,41 @@
-'use clinet';
-
 import Image from 'next/image';
+import Subscribe from '../componments/Subscribe';
 
 export default function Home() {
   return (
     <main className='flex'>
       <div className='w-[9vw]'></div>
       <div className='flex flex-col h-screen justify-around w-4/9'>
-        {/* <div className='flex-grow'></div> */}
-        <div className='mt-6 flex-grow'>
-          <div className='flex mt-8 mb-6'>
+        <div className='mt-8 flex-grow'>
+          <div className='flex mt-8'>
+            <div className='h-20 w-20 relative'>
             <Image
               src='/images/logo1.png'
               alt='bookends logo image'
-              width='80'
-              height='80'
-              // sizes='100%'
-            />
-            <h1 className='ml-3 mt-2 font-raleway text-black text-6xl font-bold '>
+              layout="fill" 
+              objectFit="cover"
+              className="rounded-sm"   />
+            </div>
+            <h1 className='ml-3 -mt-7 font-raleway text-black text-[95px] font-black '>
               bookends
             </h1>
           </div>
         </div>
 
-        <h1 className='font-raleway text-7xl font-bold mb-5'>
+        <h1 className='font-raleway text-[96px] font-bold mb-3'>
           Create. Save. Sustain.
         </h1>
-        <h1 className='font-raleway text-3xl font-light flex-grow'>
+        <h1 className='font-raleway text-[47px] font-light flex-grow'>
           Your Second-Hand Creative Marketplace.
         </h1>
 
-        <p className='flex-grow font-raleway font-light w-[75%] mb-3'>
+        <p className='flex-grow font-raleway text-[24px] font-light w-[75%] mb-3'>
           Whether you&apos;re lighting up your first YouTube scene or crafting
           cinema-scale stories, Bookends is your go-to marketplace. Here,
           creatives of all calibers connect to buy and sell pre-loved gear.
         </p>
 
-        <form className='flex-grow mb-4'>
-          <input
-            className='placeholder-font-raleway placeholder-black placeholder:font-light placeholder:text-3xl px-4 py-5 rounded-[39px] w-[57%] focus:outline-none focus:ring-2 focus:ring-red-600 focus:border-transparent text-4xl'
-            type='email'
-            placeholder='Secure Early Access: Join the waitlist!'
-          />
-        </form>
+          <Subscribe />
       </div>
 
       <div className='flex-1  no-scrollbar w-4/9'>
@@ -60,7 +53,6 @@ export default function Home() {
             alt='bookends demo image iphone13'
             width='349'
             height='716'
-            // sizes='100vw'
             className='absolute ml-2 -top-[14%] -right-[138px] drop-shadow-4xl'
           />
         </div>
---
+++ componments/Subscribe.jsx
@@ -0,0 +1,15 @@
+'use clinet';
+
+export default function Subscribe() {
+  return (
+    <>
+      <form className='flex-grow mb-4'>
+        <input
+          className='placeholder-font-raleway placeholder:font-light placeholder:text-[46px] px-4 py-4 rounded-[39px] w-[842px] focus:outline-none focus:ring-2 focus:ring-red-600 focus:border-transparent text-4xl'
+          type='email'
+          placeholder='Unlock your early access'
+        />
+      </form>
+    </>
+  );
+}
