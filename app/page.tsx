247fea027ba96c0ae66ce4017aba5078eed47334
MrJanHorak <janny_b_goode@hotmail.com>
2023-10-25 23:15:23 -0400

    add mailchimp code

--- app/page.tsx
+++ app/page.tsx
@@ -7,8 +7,8 @@ export default function Home() {
       <div className='w-[9vw]'></div>
       <div className='flex flex-col h-screen justify-around w-4/9'>
         <div className='mt-8 flex-grow'>
-          <div className='flex mt-8'>
-            <div className='h-20 w-20 relative'>
+          <div className='flex mt-[2vw]'>
+            <div className='h-[6vw] w-[6vw] relative'>
             <Image
               src='/images/logo1.png'
               alt='bookends logo image'
@@ -16,20 +16,20 @@ export default function Home() {
               height={80}
               className="rounded-sm"   />
             </div>
-            <h1 className='ml-3 -mt-8 font-raleway text-black text-[98px] font-semibold'>
+            <h1 className='ml-[.2vw] -mt-[1.6vw] font-raleway text-black text-[6vw] font-semibold'>
               bookends
             </h1>
           </div>
         </div>
 
-        <h1 className='font-raleway text-[96px] font-bold mb-3'>
+        <h1 className='font-raleway text-[5vw] font-bold mb-3'>
           Create. Save. Sustain.
         </h1>
-        <h1 className='font-raleway text-[47px] font-light flex-grow'>
+        <h1 className='font-raleway text-[3.1vw] font-light flex-grow'>
           Your Second-Hand Creative Marketplace.
         </h1>
 
-        <p className='flex-grow font-raleway text-[24px] font-light w-[60%] mb-3'>
+        <p className='flex-grow font-raleway text-[1.3vw] font-light w-[60%] mb-3'>
           Whether you&apos;re lighting up your first YouTube scene or crafting
           cinema-scale stories, Bookends is your go-to marketplace. Here,
           creatives of all calibers connect to buy and sell pre-loved gear.
@@ -46,14 +46,14 @@ export default function Home() {
             width='349'
             height='716'
             sizes='100vw'
-            className='p-1 absolute h-auto max-w-xs drop-shadow-2xl right-[245px] top-[22%]'
+            className='p-1 absolute h-auto max-w-xs drop-shadow-2xl right-[12vw] top-[20%]'
           />
           <Image
             src='/images/iPhone13.png'
             alt='bookends demo image iphone13'
             width='349'
             height='716'
-            className='p-1 absolute h-auto max-w-xs top-[5%] right-[28px] drop-shadow-2xl -z-10'
+            className='p-1 absolute h-auto max-w-xs top-[6%] right-[1.5vw] drop-shadow-2xl -z-10'
           />
         </div>
  
--- components/Subscribe.jsx
+++ components/Subscribe.jsx
@@ -1,17 +1,91 @@
 'use clinet';
 
-import '../app/globals.css'
+import '../app/globals.css';
 
 export default function Subscribe() {
   return (
     <>
-      <form className='flex-grow mb-4'>
-        <input
+
+        <div id='mc_embed_shell' className='flex-grow mb-4'>
+          <div id='mc_embed_signup' >
+            <form
+              action='https://app.us21.list-manage.com/subscribe/post?u=dcba829fef4a1a428046191ec&amp;id=953ef19679&amp;f_id=0097e4e6f0'
+              method='post'
+              id='mc-embedded-subscribe-form'
+              name='mc-embedded-subscribe-form'
+              className='validate flex'
+              target='_self'
+              noValidate=''
+            >
+              <div id='mc_embed_signup_scroll'>
+                  <input
+                    type='email'
+                    name='EMAIL'
+                    className='placeholder-font-raleway placeholder:font-light placeholder:text-[3vw] px-[1.5vw] py-[1vw] rounded-[29px] w-[47vw] focus:outline-none focus:ring-2 focus:ring-red-600 focus:border-transparent text-4xl required email'
+                    id='mce-EMAIL'
+                    required=''
+                    placeholder='Unlock your early access'
+                  />
+                <div id='mce-responses'
+                 className='clear foot'
+                 >
+                  <div
+                    id='mce-error-response'
+                    className='hidden response'
+                  ></div>
+                  <div
+                    id='mce-success-response'
+                    className='hidden response'
+                  ></div>
+                </div>
+                <div
+                  aria-hidden='true'
+                  // style='position: absolute; left: -5000px;'
+                >
+                  <input
+                  className='hidden'
+                    type='text'
+                    name='b_dcba829fef4a1a428046191ec_953ef19679'
+                    tabIndex='-1'
+                    defaultValue=''
+                  />
+                </div>
+                <div className='optionalParent'>
+                  <div className='clear foot'>
+                    <input
+                      type='submit'
+                      name='subscribe'
+                      id='mc-embedded-subscribe'
+                      className='button'
+                      value='Subscribe'
+                    />
+                    <p >
+                      <a
+                        href='http://eepurl.com/iCz-cc'
+                        title='Mailchimp - email marketing made easy and fun'
+                      >
+                        <span>
+                          <imgage
+                            className='refferal_badge'
+                            src='https://digitalasset.intuit.com/render/content/dam/intuit/mc-fe/en_us/images/intuit-mc-rewards-text-dark.svg'
+                            alt='Intuit Mailchimp'
+                          />
+                        </span>
+                      </a>
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
+
+        {/* <input
           className='placeholder-font-raleway placeholder:font-light placeholder:text-[46px] px-4 py-4 rounded-[39px] w-[842px] focus:outline-none focus:ring-2 focus:ring-red-600 focus:border-transparent text-4xl'
           type='email'
           placeholder='Unlock your early access'
-        />
-      </form>
+        /> */}
+      {/* </form> */}
     </>
   );
 }
