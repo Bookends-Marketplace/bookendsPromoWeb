a882009a4c16958a33860f0980c7e6de45df4c70
sforesi <sebastiano.foresi@gmail.com>
2023-12-18 09:06:52 -0500

    rudimentary styling and functionality

--- pages/faqs.jsx
+++ pages/faqs.jsx
@@ -4,17 +4,17 @@ import Image from 'next/image';
 
 const FAQSPage = () => {
     return (
-        <div style={{ display: 'flex', minHeight: '100vh',backgroundColor: '#f67570', }}>
-            <div style={{ flex: 1, padding: '20px', maxWidth: '300px' }}>
+        <div style={{ display: 'flex', minHeight: '100vh',backgroundColor: '#f67570', alignItems: 'center' }}>
+            <div style={{ flex: 1, padding: '168px', maxWidth: '300px' }}>
                 <Image 
                     src="/images/SignUpPage.png" 
                     alt="iPhone Prototype" 
                     layout="responsive"
-                    width={100} 
-                    height={300} 
+                    width={162} 
+                    height={330} 
                 />
             </div>
-            <div style={{ flex: 2, backgroundColor: '#f67570', padding: '20px', color: '#fffff', fontFamily: 'Raleway, sans-serif' }}>
+            <div style={{ flex: 2, backgroundColor: '#f67570', padding: '20px', color: 'aliceblue', fontFamily: 'Raleway, sans-serif' }}>
                 <h1 style={{ textAlign: 'left', marginBottom: '20px' }}>Frequently Asked Questions</h1>
                 <Accordion />
             </div>
