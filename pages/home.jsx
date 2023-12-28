e353dc682c99f7fb8ff4c89454e7881c8ea6487e
sforesi <sebastiano.foresi@gmail.com>
2023-12-28 12:41:59 -0500

    resize images on home page

--- pages/home.jsx
+++ pages/home.jsx
@@ -15,11 +15,17 @@ const NewHome = () => {
     }
   }, []);
 
+  
+
     return (
         <>
           <Nav />
           <AnimatedSection>
-            <main className='min-h-screen flex mt-12 ' >
+            <main 
+            className='min-h-screen flex mt-8 no-scrollbar' 
+            style={{
+              overflow: 'hidden',
+            }} >
               <div className='w-[2vw] sm:w-[9vw]'></div>
               <div className='flex flex-col h-screen justify-around w-full md:w-4/9'>
                 <motion.h1 
@@ -35,49 +41,53 @@ const NewHome = () => {
                     transition={{ duration: 1.5 }}
                     className='font-raleway font-light text-[4.2vw] -mt-1 mb-4 sm:mb-5 text-[3.8vw] sm:text-[2.45vw] md:text-[2.45vw] sm:grow-[.1] md:grow-[.28] lg:flex-grow'>
                         Your Creative Marketplace.
-                    {/* Join the creative marketplace where professionals and beginners alike find, sell, and share quality gear. */}
                 </motion.p>
                 <div className='visible sm:hidden'>
-                  <div className='ml-auto mr-auto flex-grow w-[40vw] h-[auto]'>
+                  <div className='ml-auto mr-auto flex-grow w-[60vw] h-[auto]'>
                     <Image
-                      src='/images/ItemPage.svg'
+                      src='/images/Search.svg'
                       alt='bookends demo image iphone14'
-                      width='183'
-                      height='389'
-                      // sizes='100vw'
+                      width={200}  
+                      height={424} 
                       className='ml-auto mr-auto drop-shadow-2xl'
                     />
                   </div>
                 </div>
     
                 <p
-                  className={`mb-4  mt-4 pr-2 sm:p-0 sm:grow-[.05] md:grow-[.2] lg:grow font-raleway font-light text-[4vw] sm:text-[2.1vw] md:text-[2vw] lg:text-[1.4vw] sm:w-[95%] sm:mb-3`}
+                  className={`mb-4  mt-4 pr-2 sm:p-0 sm:grow-[.05] md:grow-[.2] lg:grow font-raleway font-light text-[4vw] sm:text-[2.1vw] md:text-[2vw] lg:text-[2.05vw] sm:w-[95%] sm:mb-3`}
                 >
-                  Whether you&apos;re lighting up your first YouTube scene or crafting
+                  Whether you&apos;re lighting up your first scene or crafting
                   cinema-scale stories, <span className="font-bold">bookends</span> is your go-to marketplace. Here,
-                  creatives of all calibers connect to buy and sell pre-loved gear.
+                  creatives of all calibers connect to buy and sell pre-loved gear. 
+                 
                 </p>
-    
+                <p
+                 className={`pr-2 sm:p-0 sm:grow-[.05] md:grow-[.2] lg:grow font-raleway font-light text-[4vw] sm:text-[2.1vw] md:text-[2vw] lg:text-[2.05vw] sm:w-[95%] sm:mb-3`} 
+                >
+                  Sign up now for early access!
+                </p>
                 <Subscribe />
               </div>
     
               <div className='hidden sm:visible sm:flex sm:mr-5 md:visible lg:visible xl:visisble'>
-                <div className='w-[17vw] h-[auto]'>
+                <div className='hidden sm:block sm:w-2/3 lg:w-3/4 xl:w-4/5'>
                   <Image
                     src='/images/Search.svg'
                     alt='bookends demo image iphone14'
-                    width='349'
-                    height='716'
+                    width={600} 
+                    height={1200} 
                     // sizes='100vw'
                     className='relative drop-shadow-2xl right-[-5vw] top-[10.5%] md:right-[-5vw] md:top-[11.5%] lg:right-[-4vw] lg:top-[19%]'
                   />
                 </div>
-                <div className='w-[17vw] h-[auto]'>
+                {/* <div className='w-[17vw] h-[auto]'> */}
+                <div className='hidden sm:block sm:w-2/3 lg:w-3/4 xl:w-4/5'>
                   <Image
                     src='/images/ItemPage.svg'
                     alt='bookends demo image iphone13'
-                    width='349'
-                    height='716'
+                    width={600} 
+                    height={1200} 
                     className='relative top-[6%] right-[1.5vw] md:top-[6%] md:right-[1.5vw] lg:top-[8%] lg:right-[1.5vw] drop-shadow-2xl -z-10'
                   />
                 </div>
