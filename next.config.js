fd132e6da2d1f78168f3d862c4d32b5feb5fe23b
MrJanHorak <janny_b_goode@hotmail.com>
2024-06-28 22:48:20 -0400

    begin to add assetlink files for android and ios

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/.well-known/:file',
+        destination: '/api/.well-known/:file',
+        permanent: false,
+      },
+    ]
+  },
+}
 
 module.exports = nextConfig
